c1ab3b1693f099b152778ab4aac0e410ddfc39d4
Maciej <maciej@romanowski.it>
2020-03-19 17:16:18 +0100

    Initial commit

---
+++ src/main/java/it/romanowski/ProductOrderExample.java
@@ -0,0 +1,12 @@
+package it.romanowski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderExample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOrderExample.class, args);
+    }
+}
